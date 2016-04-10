2da4cb1d9418edb42e1c2435b055402a9f3d3c19
divyatechryde <divya@techryde.com>
2016-04-10 15:00:20 +0530

    Update before-install.bat

--- before-install.bat
+++ before-install.bat
@@ -1,4 +1,4 @@
 REM Install Internet Information Server (IIS). 
 REM c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
 REM c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
-REM IISRESET
+IISRESET
